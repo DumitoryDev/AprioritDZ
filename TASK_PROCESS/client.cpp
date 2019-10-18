b11694b2cad08ba1bcf144d8d1a129ca1f94d2d5
Dmitry <45358107+DumitoryDev@users.noreply.github.com>
2019-10-18 22:04:40 +0300

    add new task

---
+++ TASK_PROCESS/client.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <boost/asio.hpp>
+#include <string>
+#include <fstream>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/binary_oarchive.hpp>
+#include <filesystem>
+
+
+struct info_file
+{
+	friend class boost::serialization::access;
+	std::wstring name_file{};
+	std::size_t length_file{};
+
+	template<class Archive>
+	void serialize(Archive& ar, const unsigned int version)
+	{
+		ar& this->name_file;
+		ar& this->length_file;
+	}
+
+}; 
+
+int wmain(int argc, wchar_t ** argv)
+{
+	try
+	{
+		if (argc == 1)
+		{
+			throw std::runtime_error("Invalid arguments!");
+		}
+		
+		std::wstring path_file(argv[1]);
+				
+		if (path_file.empty())
+		{
+			throw std::runtime_error("Path is empty!");
+		}
+
+		std::ifstream file(path_file, std::ios::binary);
+		if (!file.is_open())
+		{
+			throw std::runtime_error("Error open file!");
+		}
+		
+		std::wcout<<L"File is open!"<<std::endl;
+		info_file info_file{};
+		auto size_file = std::filesystem::file_size(path_file);
+		info_file.name_file = path_file;
+		info_file.length_file = size_file;
+		
+		std::wcout<<L"Type ip: "<<std::endl;
+		std::string ip{ "192.168.1.11" };
+		std::getline(std::cin, ip);
+		
+		if (ip.empty())
+		{
+			throw std::runtime_error("Ip is empty!");
+		}
+		
+		std::wcout<<L"Type port: "<<std::endl;
+		unsigned short port{ 33333 };
+		std::cin >> port;
+		
+		boost::asio::io_service io_service;
+		boost::asio::ip::tcp::endpoint end{ boost::asio::ip::address::from_string(ip),port};
+		boost::asio::ip::tcp::iostream stream;
+		
+		std::wcout << L"Connect to server...." << std::endl;
+		stream.connect(end);
+		std::wcout << L"Ok!" << std::endl;
+			
+		{ 
+			boost::archive::text_oarchive os(stream);
+			std::wcout<<L"Send info..."<<std::endl;
+			os << info_file;
+			std::wcout<<L"OK!"<<std::endl;
+		}
+		
+		const size_t size_packet = 4096;
+		std::size_t full_size{};
+		
+		boost::archive::text_oarchive oarch( stream, boost::archive::no_header );
+		while (!file.eof())
+		{
+			std::string data(size_packet,'\0');
+
+			file.read(data.data(),data.size());
+
+			if (!file.gcount())
+			{
+				throw std::runtime_error("Fail read file!");
+			}
+			data.resize(file.gcount());
+		    oarch << data;
+			full_size+=file.gcount();
+			std::wcout<<L"\rsize packet - "<<full_size;
+			
+		}
+			
+		std::wcout<<L"\nFile received!"<<std::endl;
+	}
+	catch (const std::exception & error)
+	{
+		std::wcout<<error.what()<<std::endl;
+	}
+	catch(...)
+	{
+		std::wcout<<"Error..."<<std::endl;
+	}
+
+}
+
+
---
+++ TASK_PROCESS/server.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/binary_iarchive.hpp>
+#include <fstream>
+
+using boost::asio::ip::tcp;
+
+struct info_file
+{
+	friend class boost::serialization::access;
+	std::wstring name_file{};
+	std::size_t length_file{};
+
+	template<class Archive>
+	void serialize(Archive & ar, const unsigned int version)
+	{
+		ar & this->name_file;
+		ar & this->length_file;
+	}
+	
+};
+
+int wmain(void)
+{
+	try
+	{
+		unsigned short port = 33333;
+		
+		boost::asio::io_service io_service;
+		tcp::iostream stream;
+        tcp::acceptor a { io_service, tcp::endpoint { tcp::v4(), port } };
+		tcp::socket sock{io_service};
+		
+		std::wcout<<L"Server started...\n";
+		a.accept(*stream.rdbuf());
+		std::wcout<<L"Connected!\n";
+		
+		info_file info_file{};
+		{
+			boost::archive::text_iarchive oa(stream);
+			oa >> info_file;
+		}
+
+		std::ofstream file(info_file.name_file,std::ios::binary | std::ios::trunc);
+
+		if (!file.is_open())
+		{
+			throw std::runtime_error("Error create file!");
+		}
+
+		std::wcout<<L"Start receive..\n";
+		
+		size_t full_size = 0;
+		const size_t size_packet = 4096;
+		
+		boost::archive::text_iarchive iarch(stream, boost::archive::no_header );
+		
+		while (full_size != info_file.length_file)
+		{
+			
+			std::string str(size_packet,'\0');
+			iarch >> str;
+			file.write(str.data(),str.size());
+			full_size+=str.length();
+			std::wcout<<L"\rsize packet - "<<full_size;
+					
+		}
+		std::wcout<<L"\nFile received!"<<std::endl;
+		
+		
+	}
+	catch (const std::exception & err)
+	{
+		std::wcout<<err.what();
+	}
+	catch(...)
+	{
+		std::wcout<<L"Error..."<<std::endl;
+	}
+		
+}
